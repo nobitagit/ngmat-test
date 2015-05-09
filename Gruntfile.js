bc1fb16ace992ab244b82e7462b3445c43ee86d1
Nobita <aurelio.ogliari@gmail.com>
2015-05-09 08:52:17 +0100

    updates

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'app/assets/app.css': 'app/assets/app.scss',       // 'destination': 'source'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass']);
+
+};
